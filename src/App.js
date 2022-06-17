9c8eefa43d67c5ea05de0a3832dbca40b97ba8f7
Alejae1998 <alejagr1998@gmail.com>
2022-06-16 20:33:46 -0700

    adding code to app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [allMovies, setAllMovies] = useState([]);
+  const [filteredMovies, setAllFilteredMovies] = useState([]);
+  const [movieFormName, setMovieFormName] = useState([]);
+  const [movieFromYearReleased, setMovieFromYearReleased] = useState('');
+  const [movieFormDirector, setMovieFormDirector] = useState('');
+  const [movieFormColor, setMovieFormColor] = useState('lightgreen');
+  const [filterQuery, setFilterQuery] = useState('');
+
+  useEffect(() => handleFilterMovies(filterQuery), [allMovies, filterQuery]);
+
+  function submitMovie(e) {
+    e.preventDefault();
+    const newMovie = {
+      name: movieFormName,
+      year: movieFromYearReleased,
+      color: movieFormColor,
+      director: movieFormDirector,
+    };
+    setAllMovies([...allMovies, newMovie]);
+
+    setMovieFormName('');
+    setMovieFromYearReleased('');
+    setMovieFormDirector('');
+    setMovieFormColor('');
+  }
+
+  function handleDeleteMovie(name) {
+    const movieIndex = allMovies.findIndex((movie) => movie.name === name);
+
+    allMovies.splice(movieIndex, 1);
+
+    setAllMovies([...allMovies]);
+  }
+
+  function handleFilterMovies(search) {
+    const searchMovies = allMovies.filter((movie) => movie.name.includes(search));
+
+    search ? setAllFilteredMovies(searchMovies) : setAllFilteredMovies(allMovies);
+  }
   return (
     <div className="App">
       <header className="App-header">
